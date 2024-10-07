5953888f3ec3c5b4a9c3f3f3da432f517a353b99
Artur <artur.schneider@biologie.uni-freiburg.de>
2024-10-07 22:09:02 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="FreiCtrl_Laser",
-    version='0.0.1',
+    version='0.0.5',
     description='Laser controller for optogenetic experiments',
     author='Artur',
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
-            'pyqt6~=6.4',
-            'pyqtgraph~=0.13.3',
+            'pyqt6~=6.4.2',
+            'pyqtgraph~=0.13.1',
             'pyserial'
         ],
     packages=find_packages(include=["FreiCtrl_laser"], exclude=['docs', 'circuitpython_code, arduino_barcodes'])
